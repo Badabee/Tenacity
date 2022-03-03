d55e6969c62a4714d30dfbaf7359907d86e37dea
badabee <badabee440@gmail.com>
2022-03-03 23:21:20 +0100

    Images, responsiveness and js

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-const navigation = document.querySelector("#icon")
-onclick
+
+function myFunction() {
+    var navs = document.getElementById("#icon");
+    var links = document.getElementById("#mylinks");
+    
+    if(links.style.display === "none"){
+        links.style.display = "block";
+    } else {
+        links.style.display = "none";
+      }
+
+    }
